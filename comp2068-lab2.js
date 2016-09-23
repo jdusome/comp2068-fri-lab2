8352a0577533c1f9bfee5cdd25f15c960bb65a72
jdusome <200236108@student.georgianc.on.ca>
2016-09-23 00:14:58 -0400

    lab2 rock paper scissors

---
+++ comp2068-lab2.js
@@ -0,0 +1,82 @@
+// Created By Joshua Dusome
+
+// declare variable userChoice, prompt user to enter choice
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+
+// declare variable computerChoice as random number
+var computerChoice = Math.random();
+
+// evaluate computer choice as rock, paper or scissors
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+
+    // Post computers choice to the screen
+} console.log("Computer: " + computerChoice);
+
+// Compare choices to find a winner
+function compare(choice1, choice2) {
+
+    // If results are the same, its a tie
+    if (choice1 === choice2) {
+
+        return ("The result is a tie!")
+
+    }
+// If the user chooses rock
+    else if (choice1 === "rock"){
+
+        if (choice2 === "scissors"){
+
+            return ("rock wins")
+
+        }
+
+        else {
+
+            return ("paper wins")
+
+        }
+
+    }
+
+//If the user chooses paper
+    else if (choice1 === "paper"){
+
+        if (choice2 === "rock"){
+
+            return ("paper wins")
+
+        }
+
+        else {
+
+            return ("scissors wins")
+
+        }
+
+    }
+//If the user chooses scissors
+    else if (choice1 === "scissors"){
+
+        if (choice2 === "rock"){
+
+            return ("rock wins")
+
+        }
+
+        else {
+
+            return ("scissors wins")
+
+        }
+
+    }
+
+}
+
+//Pass our variables into the compare function
+compare(userChoice,computerChoice);
